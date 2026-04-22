c4a28c535d32a49c5c760ec3999fc63a2fbdc3e0
Borut Tomazin <borut@povio.com>
2026-04-22 13:27:37 +0200

    Package update.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
   name: "PovioKit",
   platforms: [
-    .iOS(.v16),
-    .macOS(.v13)
+    .iOS(.v17),
+    .macOS(.v14)
   ],
   products: [
     .library(name: "PovioKitCore", targets: ["PovioKitCore"]),
@@ -80,6 +80,5 @@ let package = Package(
         .process("Resources/")
       ]
     ),
-  ],
-  swiftLanguageVersions: [.v5]
+  ]
 )
